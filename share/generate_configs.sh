f5431ae44d0313051e46ed6af6c96397773a02e7
Alistair <Eedles@github.com>
2023-12-04 17:48:51 +1100

    updating script templates

--- share/generate_configs.sh
+++ share/generate_configs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=generate_configs3
+#SBATCH --job-name=generate_configs
 #SBATCH --output=./logs/%x.log
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=20
@@ -15,8 +15,8 @@
 ml gcc/11.3.0 openmpi/4.1.4 python/3.10.4 cudnn/8.4.1.50-cuda-11.7.0 git/2.36.0
 source /fred/oz016/damon/envs/nt_310/bin/activate
 
-# cd "/fred/oz016/alistair/GWSamplegen"
-cd "/fred/oz016/damon/GWSamplegen"
+cd "/fred/oz016/alistair/GWSamplegen"
 
-python generate_configs.py --config-file=args3.json
+
+python generate_configs.py --config-file=args.json
 #--config-file=configs/test1/args.json
