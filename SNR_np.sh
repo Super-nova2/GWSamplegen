5f5f3c4b820c17835ff0dbe4aef6d5c8f593b629
Damon Beveridge <damon.beveridge@research.uwa.edu.au>
2024-01-18 16:05:28 +1100

    Switching template selection, SNR calculation, and injection generation for samples to use the detector frame masses instead of the source frame. Also enabling these scripts to work with the BBH parameter space.

--- SNR_np.sh
+++ SNR_np.sh
@@ -3,15 +3,20 @@
 #SBATCH --output=./logs/%x_%a.log
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=10
-#SBATCH --time=10:00:00
-#SBATCH --mem=160gb
-#SBATCH --array=0-9
+#SBATCH --time=2:00:00
+#SBATCH --mem=50gb
+#SBATCH --array=0
+##SBATCH --array=0-9
 
-source /home/amcleod/.bashrc
+# source /home/amcleod/.bashrc
 
-cd "/fred/oz016/alistair/GWSamplegen"
+ml gcc/11.3.0 openmpi/4.1.4 python/3.10.4 cudnn/8.4.1.50-cuda-11.7.0 git/2.36.0
+source /fred/oz016/damon/envs/nt_310/bin/activate
+
+# cd "/fred/oz016/alistair/GWSamplegen"
+cd "/fred/oz016/damon/GWSamplegen"
 
 echo starting job
 
 #$SLURM_ARRAY_TASK_COUNT
-python asyncSNR_np.py --index=$SLURM_ARRAY_TASK_ID --totaljobs=$SLURM_ARRAY_TASK_COUNT --config-file=configs/real_dsens_300_mpc_glitch_week2/args.json
+python asyncSNR_np.py --index=$SLURM_ARRAY_TASK_ID --totaljobs=$SLURM_ARRAY_TASK_COUNT --config-file=configs/test50/args.json
